60016289babf7334b450259291c90e8033834c3b
Ryohei Ueda <garaemon@gmail.com>
2024-11-02 12:15:40 -0700

    Add a feature to set track num automatically

--- editor.py
+++ editor.py
@@ -10,6 +10,15 @@ import streamlit as st
 
 def show_ui(files: List[Path]):
     st.header('MP3 Tag Editor')
+    with st.form('Utility Script'):
+        auto_track = st.form_submit_button('Set track # automatically')
+        if auto_track:
+            for i, f in enumerate(files):
+                audio = eyed3.load(f.as_posix())
+                audio.tag.track_num = i
+                audio.tag.save()
+                st.success(f'Successfully update {f.name}')
+
     for f in files:
         audio = eyed3.load(f.as_posix())
         if not audio.tag:
